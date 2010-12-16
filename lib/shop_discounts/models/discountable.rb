2f787c2f92a6feb812c91f8c5bdfb7249f5112bc
Dirk Kelly <dk@dirkkelly.com>
2010-12-16 16:07:23 +0800

    destroy discounts on a discountable destruction

--- lib/shop_discounts/models/discountable.rb
+++ lib/shop_discounts/models/discountable.rb
@@ -4,7 +4,7 @@ module ShopDiscounts
       
       def self.included(base)
         base.class_eval do
-          has_many  :discountables, :class_name => 'ShopDiscountable', :foreign_key  => :discounted_id
+          has_many  :discountables, :class_name => 'ShopDiscountable', :foreign_key  => :discounted_id, :dependent => :destroy
           has_many  :discounts,     :class_name => 'ShopDiscount',     :through      => :discountables          
         end
       end
