044c7d553e8ed03cf6d9fa9df9e7328a0e072563
Dirk Kelly <dk@dirkkelly.com>
2011-01-11 09:20:18 +0800

    new files for discountable custom logic

---
+++ lib/shop_discounts/models/order_discountable.rb
@@ -0,0 +1,14 @@
+module ShopDiscounts
+  module Models
+    module OrderDiscountable
+      
+      def self.included(base)
+        base.class_eval do
+          has_many  :discountables, :class_name => 'OrderShopDiscountable', :foreign_key  => :discounted_id, :dependent => :destroy
+          has_many  :discounts,     :class_name => 'ShopDiscount',          :through      => :discountables          
+        end
+      end
+      
+    end
+  end
+end
