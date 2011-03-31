89a6aa8ddd40b35f10ef85d2ed827c9573613233
Dirk Kelly <dk@dirkkelly.com>
2011-04-01 02:08:27 +0800

    use decimals, calculate tax based on discounts.

--- lib/shop_discounts/models/purchaseable.rb
+++ lib/shop_discounts/models/purchaseable.rb
@@ -1,33 +1,53 @@
 module ShopDiscounts
   module Models
     module Purchaseable
-      
+
       def self.included(base)
         base.class_eval do
-          
+
           def discount
             discount = BigDecimal.new('0.00')
             discounts.map { |d| discount += d.amount }
-            
+
             # Maximum discount is 100%
-            discount = [discount,100.0].min
-            
+            discount = [discount,BigDecimal.new('100.0')].min
+
             # Convert to a percentage
-            discount * 0.01
+            discount * BigDecimal.new('0.01')
           end
-          
+
           def discounted
-            (value * discount)
+            (rrp * discount)
           end
-          
-          alias_method :value, :price
+
+          alias_method :rrp, :cost
+          def cost
+            cost = (item_price * quantity) - ((item_price * quantity) *discount)
+            cost -= tax if Radiant::Config['shop.tax_strategy'] === 'exclusive'
+            cost
+          end
+
+          def tax
+            percentage = Radiant::Config['shop.tax_percentage'].to_f * 0.01
+
+            case Radiant::Config['shop.tax_strategy']
+            when 'inclusive'
+              tax = price - (price / (1 + percentage))
+            when 'exclusive'
+              tax = price * percentage
+            else
+              tax = 0
+            end
+
+            BigDecimal.new(tax.to_s)
+          end
+
           def price
-            result = BigDecimal.new('0.00')
-            result = value - discounted
+            rrp - discounted
           end
         end
       end
-      
+
     end
   end
 end
