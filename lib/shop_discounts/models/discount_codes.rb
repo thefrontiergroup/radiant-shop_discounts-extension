893f5152902713ca356c056b3addfd884533fcc6
Mario Visic <mario@mariovisic.com>
2011-09-19 12:51:02 +0800

    More testing for the shop discount codes.

--- lib/shop_discounts/models/discount_codes.rb
+++ lib/shop_discounts/models/discount_codes.rb
@@ -4,17 +4,16 @@ module ShopDiscounts
 
       def self.included(base)
         base.class_eval do
-
           before_save :add_discount_code if :discount_code_changed?
-
         end
       end
 
       def add_discount_code
-        if discounts.blank?
-          discount = ShopDiscount.find(:first, :conditions => { :code => discount_code })
-          if discount.present?
-            discounts << discount
+        if discounts.blank? && discount_code.present?
+          # discount = ShopDiscount.find(:first, :conditions => { :code => discount_code })
+          discountable = item.discountables.find(:first, :joins => :discount, :conditions => { :shop_discounts => { :code => discount_code } })
+          if discountable.present?
+            discounts << discountable.discount
           end
         end
       end
--- spec/models/discount_codes_spec.rb
+++ spec/models/discount_codes_spec.rb
@@ -10,17 +10,43 @@ describe ShopDiscounts::Models::DiscountCodes do
 
     let(:shop_line_item) { shop_line_items(:one) }
     let(:shop_discount)  { shop_discounts(:ten_percent) } # Discount code: 10pcoff
+    let(:item)           { shop_line_item.item }
 
     describe '#add_discounts' do
 
       context 'when the discount code matches an actual discount' do
 
-        before :each do
-          shop_line_item.update_attributes(:discount_code => '10pcoff')
+        context 'when the discount applies to the product' do
+
+          before :each do
+            shop_line_item.discountables.destroy_all
+            item.discountables.destroy_all
+            item.discounts << shop_discount
+
+            shop_line_item.discountables.destroy_all
+            shop_line_item.update_attributes(:discount_code => '10pcoff')
+          end
+
+          it 'should add a discount' do
+            shop_line_item.discounts.should include shop_discount
+          end
+
         end
 
-        it 'should add a discount' do
-          shop_line_item.discounts.should include shop_discount
+        context 'when the discount does not apply to the product' do
+
+          before :each do
+            shop_line_item.discountables.destroy_all
+            item.discountables.destroy_all
+
+            shop_line_item.discountables.destroy_all
+            shop_line_item.update_attributes(:discount_code => '10pcoff')
+          end
+
+          it 'should not add a discount' do
+            shop_line_item.discounts.should be_blank
+          end
+
         end
 
       end
@@ -28,13 +54,13 @@ describe ShopDiscounts::Models::DiscountCodes do
       context 'when the discount code does not match a discount' do
 
         before :each do
+          shop_line_item.discountables.destroy_all
           shop_line_item.update_attributes(:discount_code => 'not_a_discount_code')
         end
 
         it 'should not add a discount' do
           shop_line_item.discounts.should be_blank
         end
-
 
       end
 
